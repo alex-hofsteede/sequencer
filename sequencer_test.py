225966451716fcf1fc02055c47096f6f08f0cdef
alex-hofsteede <alex@hofsteede.com>
2013-05-21 00:21:20 -0700

    Add tests

---
+++ sequencer_test.py
@@ -0,0 +1,49 @@
+import sequence
+import unittest
+
+class TestSequence(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def test_number_sequences(self):
+        self.assertEqual(sequence.recognise(['mon', 'tue', 'wed']), 'weekdays')
+        self.assertEqual(sequence.recognise([1,2,-3]), 'integers') #no pattern, should still recognise type
+        self.assertEqual(sequence.recognise([1.0,2.0,-3.0]), 'floats') #no pattern, should still recognise type
+        #self.assertEqual(sequence.recognise(['jan', 'february', 'mar']), 'months') #mixed types
+        self.assertEqual(sequence.recognise(['a','m','j','j']), 'months')
+        self.assertEqual(sequence.recognise(['m','t','w','t','f','m','t','w','t','f']), 'weekdays')
+        self.assertEqual(sequence.recognise(['boo','barf','derp']), None)
+        self.assertEqual(sequence.recognise(['23:45','23:50','23:55']), 'hour:min')
+        self.assertEqual(sequence.recognise(['23:45:01','23:50:02','23:55:03']), 'hour:min:sec')
+
+    def test_detect_repeats(self):
+        self.assertEqual(sequence.detect_repeats([1,2,3,1,2,3,1,2,3,1,2,3]), [1,2,3]) #full repeated sequence
+        self.assertEqual(sequence.detect_repeats([2,4,6,2,5,6,2,4,6]), None) #sequence broken
+        self.assertEqual(sequence.detect_repeats([2,4,6,2,4,6,2,4]), [2,4,6]) #sequence not evenly divisible, has remainder
+        self.assertEqual(sequence.detect_repeats([2,4,6,2,4,6,2,5]), None) #sequence breaks in remainder portion
+        self.assertEqual(sequence.detect_repeats([5,5,3,5,5,5,5,3,5,5,5,5]), [5,5,3,5,5]) #repeated subsequences in main sequence
+
+    def test_pattern(self):
+        self.assertEqual(sequence.pattern([1,2,3]), ([[1],[1.0]], sequence.SEQ_ARITHMETIC ))
+        self.assertEqual(sequence.pattern([2,4,6,8]), ([[2],[2.0]], sequence.SEQ_ARITHMETIC ))
+        self.assertEqual(sequence.pattern([1,2,3,1,2,3]), ([[1,2,3]], sequence.SEQ_REPEATING ))
+        self.assertEqual(sequence.pattern([1,2,4,5,7,8]), ([[1],[1.0,2.0]], sequence.SEQ_ARITHMETIC ))
+        self.assertEqual(sequence.pattern([1,2,4,8,16,32]), ([[1],[2.0]], sequence.SEQ_GEOMETRIC )) #Detect geometric sequence with step 2
+        self.assertEqual(sequence.pattern([1,-2,4,-8,16,-32]), ([[1],[-2.0]], sequence.SEQ_GEOMETRIC )) #Detect geometric sequence with negative step
+        #self.assertEqual(sequence.pattern([1,2,4,7,11]), ([[1],[1],[1.0]], sequence.SEQ_ARITHMETIC )) #Detect increment in step length
+
+    def test_extend(self):
+        self.assertEqual(sequence.extend(['N', 'O', 'F'], 3), ['Ne', 'Na', 'Mg']) #N,O,F could be months or elements, choose elements based on the better pattern match
+        self.assertEqual(sequence.extend([1,2,3], 3), [4,5,6])
+        self.assertEqual(sequence.extend(['M','T','W','T','F','S'], 3), ['S','M','T']) #test wrapping of pattern based on the match data, not the sequence itself. 
+        self.assertEqual(sequence.extend([1.2,1.4,1.6], 3), [1.8,2.0,2.2])
+        self.assertEqual(sequence.extend([1,2,4,5,7], 3), [8,10,11])
+        self.assertEqual(sequence.extend(['He', 'Li', 'Be'], 3), ['B', 'C', 'N'])
+        self.assertEqual(sequence.extend(['01:45','01:50','01:55'], 2), ['02:00','02:05'])
+        self.assertEqual(sequence.extend(['10:45','11:50','12:55'], 2), ['14:00','15:05'])
+        self.assertEqual(sequence.extend(['23:45','23:50','23:55'], 2), ['00:00','00:05'])
+        self.assertEqual(sequence.extend(['23:45:01','23:50:02','23:55:03'], 2), ['00:00:04','00:05:05'])
+
+if __name__ == '__main__':
+    unittest.main()
